b8c5576e81b10002cf9f2b7c45777dcc0a7308b9
Darshak Kakani <88178092+DarshaK1Just@users.noreply.github.com>
2022-07-08 23:20:05 +0530

    Add files via upload

---
+++ Note Taking App By React/src/components/Notes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Notes() {
+  return (
+    <div className="note">
+    <h1>This is the React Bootcamp</h1>
+    <p>All the students who compleate the Bootcamp with compleate attendence and project
+      submission will be rewarded certificate from us!!
+    </p>
+    </div>
+    
+  );
+}
+
+export default Notes;
